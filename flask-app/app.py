61d36af9f6d2b4696f618835a6556a5c2e675648
aeslava97 <a.eslava@uniandes.edu.co>
2020-02-17 13:30:28 -0500

    footer arreglado

--- flask-app/app.py
+++ flask-app/app.py
@@ -163,4 +163,4 @@ def binary_file():
 
 # The host='0.0.0.0' means the web app will be accessible to any device on the network
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=5000)
